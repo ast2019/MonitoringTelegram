675b7f08e997099fe321bf08542d8860df3f294d
Michél Popp <michel_popp@web.de>
2018-01-26 18:37:47 +0100

    fix host issue

--- check.sh
+++ check.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-DateString = $(date "+%d.%m.%y %H:%M") 
+#DateString = $(date "+%d.%m.%y %H:%M") 
 
 IFS=";" read -ra hosts < srv.hosts
 
 for i in ${hosts[@]}
 do
-  if ping -c 1 -w 1 $ > /dev/null 
+  if ping -c 1 -w 1 ${hosts[$i] > /dev/null 
   then
     # send telegram notifaction
     echo ${hosts[$i]}' down'
