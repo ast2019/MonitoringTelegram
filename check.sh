2ed6251a457b94d86ff6b83ff6dfb51f875b8530
Michél Popp <michel_popp@web.de>
2018-01-26 18:50:27 +0100

    update

--- check.sh
+++ check.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
 #DateString = $(date "+%d.%m.%y %H:%M") 
 
-IFS=";" read -ra hosts < srv.hosts
+IFS=";" read -ra hosts <<< "srv.txt"
 
-for i in ${hosts[@]}
-do
-  if ping -c 1 -w 1 ${hosts[$i] > /dev/null 
+for i in ${hosts[@]} ; do
+  if ping -c 1 -w 1 ${hosts[$i]} > /dev/null 
   then
     # send telegram notifaction
     echo ${hosts[$i]}' down'
   else
     echo ${hosts[$i]}' up'
   fi
+
 done
