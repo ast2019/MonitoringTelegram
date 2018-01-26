285b42bfed3336ee0d417118e2d154ea6f1b590d
Michél Popp <michel_popp@web.de>
2018-01-26 18:55:10 +0100

    .

--- check.sh
+++ check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #DateString = $(date "+%d.%m.%y %H:%M") 
-IFS=";" read -ra hosts <<< srv.txt
+IFS=";" read -ra hosts <<< 'srv.txt'
 
 for i in ${hosts[@]} ; do
   if ping -c 1 -w 1 ${hosts[$i]} > /dev/null 
